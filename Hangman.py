e8f0cc101acb0a4b65d989f6a1fe7812a017a6ed
Jacob Hall <jakeehall@gmail.com>
2016-02-08 15:30:26 -0700

    Updated ASCII Art and Improved Efficency

--- Hangman.py
+++ Hangman.py
@@ -1,6 +1,6 @@
 #Jacob Hall
 #February 3rd, 2016
-#Hangman v1.1
+#Hangman v1.2
 #GNU General Public License v3.0
 
 from random import randint
@@ -11,7 +11,7 @@ wordWithBlanks = []
 for x in hangmanWord:
     wordWithBlanks.append("_")
 
-art = ["  ————\n     |\n     |\n     |\n——————","  ————\n  0  |\n     |\n     |\n——————","  ————\n  0  |\n /   |\n     |\n——————","  ————\n  0  |\n / \ |\n     |\n——————","  ————\n  0  |\n /|\ |\n     |\n——————","  ————\n  0  |\n /|\ |\n /   |\n——————","  ————\n  0  |\n /|\ |\n / \ |\n——————"]
+art = ["  ————\n     |\n     |\n     |\n——————","  ————\n  O  |\n     |\n     |\n——————","  ————\n  O  |\n /   |\n     |\n——————","  ————\n  O  |\n / \ |\n     |\n——————","  ————\n  O  |\n /|\ |\n     |\n——————","  ————\n  O  |\n /|\ |\n /   |\n——————","  ————\n  0  |\n /|\ |\n / \ |\n——————"]
 currentArt = 0
 
 correctLetters = 0
@@ -46,8 +46,10 @@ def guess(letter):
             
 def getInput():
     userInput = input("Please guess a letter.\n")
-    while not (len(userInput) == 1) or not ((ord(userInput) >= 65 and ord(userInput) <= 90) or (ord(userInput) >= 97 and ord(userInput) <= 122)):
-        input("Please guess a valid letter.\n")
+    chrValue = ord(userInput)
+    while not (len(userInput) == 1) or not ((chrValue >= 65 and chrValue <= 90) or (chrValue >= 97 and chrValue <= 122)):
+        userInput = input("Please guess a valid letter.\n")
+        chrValue = ord(userInput)
     guess(userInput)
 
 print(art[currentArt])
